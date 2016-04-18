4c3b3fbfb03b057c253055b94f147616b73641aa
arifcakiroglu <cakirogluarif@gmail.com>
2016-04-18 18:30:13 +0300

    created clipboard app

---
+++ clipboard/main.js
@@ -0,0 +1,21 @@
+/*
+ * Nodeschool Istanbul
+ * author: Arif Cakiroglu <arif@linux.com> (http://github.com/arifcakiroglu)
+ */
+
+const app = require( 'app' );
+const BrowserWindow = require( 'browser-window' );
+
+let mainWindow = null;
+
+app.on( 'ready', function() {
+
+  let options = {
+    width: 600,
+    height: 400,
+    title : "Clipboard Example"
+  };
+
+  mainWindow = new BrowserWindow(options);
+  mainWindow.loadURL( 'file://' + __dirname + '/index.html' );
+});
