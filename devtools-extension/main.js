15d0ff09920d2cf3d34f2e6c669d92d218365af2
arifcakiroglu <cakirogluarif@gmail.com>
2016-04-18 17:48:04 +0300

    created devtools extension

---
+++ devtools-extension/main.js
@@ -0,0 +1,21 @@
+/*
+ * Nodeschool Istanbul
+ * author: Arif Cakiroglu <arif@linux.com> (http://github.com/arifcakiroglu)
+ */
+
+const app = require( 'app' );
+const BrowserWindow = require( 'browser-window' );
+
+let mainWindow = null;
+
+app.on( 'ready', function() {
+
+  let options = {
+    width: 900,
+    height: 600,
+    title : "Electron app"
+  };
+
+  mainWindow = new BrowserWindow(options);
+  mainWindow.loadURL( 'file://' + __dirname + '/index.html' );
+});
